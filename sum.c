d94e0b65b02e75a2d7083d2fd55713b7ad2d3311
AhmadMurtaza1372 <ahmadmurtazakhan123@gmail.com>
2026-04-19 15:48:08 +0530

    C

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int a,b,sum;
+    printf("enter the number :");
+    scanf("%d",&a);
+    printf("enter the another number :");
+    scanf("%d",&b);
+
+    sum= a+b;
+    printf("the sum of no is %d",sum);
+    return 0;
+}
